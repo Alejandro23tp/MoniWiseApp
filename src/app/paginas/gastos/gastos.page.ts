62105b116ed2e95eb106ea4497359b68082243a6
KleberMera <klebermera2016@gmail.com>
2024-06-19 01:10:11 -0500

    Pagina de gastos en proceso

--- src/app/paginas/gastos/gastos.page.ts
+++ src/app/paginas/gastos/gastos.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import { ModalcontentComponent } from 'src/app/componentes/modalcontent/modalcontent.component';
 import { GastosService } from 'src/app/servicios/gastos.service';
 import { GeneralService } from 'src/app/servicios/general.service';
+import { RegistrosService } from 'src/app/servicios/registros.service';
 
 @Component({
   selector: 'app-gastos',
@@ -10,13 +11,48 @@ import { GeneralService } from 'src/app/servicios/general.service';
   styleUrls: ['./gastos.page.scss'],
 })
 export class GastosPage implements OnInit {
+  usuario_id: number = 0;
+  usuario_nombre: string = '';
+  sueldoFijo: number = 0;
+
+  listaSueldoFijos: any[] = [];
   
   constructor(
+    private srvGastos: GastosService,
+    private srvGeneral: GeneralService,
+    private modalController: ModalController,
+    private modal: ModalController,
+    private srvRegistro: RegistrosService,
     
   ) {}
 
   ngOnInit() {
+    this.verificarSueldoFijo();
    
+  }
+
+
+  verificarSueldoFijo() {
+    const usuarioLogueado = localStorage.getItem('usuarioLogueado');
+    if (usuarioLogueado != null) {
+      const usuarioLogueadoObj = JSON.parse(usuarioLogueado).id;
+      this.usuario_id = usuarioLogueadoObj;
+      const usuarioLogueadoObj2 = JSON.parse(usuarioLogueado).nombre;
+      this.usuario_nombre = usuarioLogueadoObj2;
+      this.srvRegistro
+        .verSueldoFijoPorUsuario(this.usuario_id)
+        .subscribe((res: any) => {
+          if (res.data.length > 0) {
+            this.sueldoFijo = res.data[0].monto;
+            //recorrer res.data desde [0] hasta el ultimo con forEach
+            res.data.forEach((sueldoFijo: any) => {
+              this.listaSueldoFijos.push(sueldoFijo);
+            });
+            console.log('Sus Lista Sueldo Fijos: ' ,this.listaSueldoFijos);
+          
+          } 
+        });
+    }
   }
 
  
--- src/app/servicios/gastos.service.ts
+++ src/app/servicios/gastos.service.ts
@@ -8,6 +8,22 @@ import { GeneralService } from './general.service';
 export class GastosService {
   constructor(private http: HttpClient, private srvG: GeneralService) {}
 
-  
 
+  verGastos(){
+    let url = 'verGastos'
+    return this.http.get<any>(this.srvG.URLAPI + url);
+  }
+
+
+  registrarGasto(ObjetoGasto: any){
+    let url = 'registrarGasto'
+    return this.http.post(this.srvG.URLAPI + url, this.srvG.objectToFormData({
+      monto : ObjetoGasto.monto,
+      fecha : ObjetoGasto.fecha,
+      descripcion : ObjetoGasto.descripcion,
+      categoria_id : ObjetoGasto.categoria_id,
+      usuario_id : ObjetoGasto.usuario_id,
+      estado_pago : ObjetoGasto.estado_pago,
+    }));
+  }
 }
